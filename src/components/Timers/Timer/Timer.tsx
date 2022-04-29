6f45c0325d3a625a244b94f1f19a36b6634ee7f4
bittmario <jamario@bitt.com>
2022-04-28 20:51:11 -0400

    Session2. Circular progress bar initial styles.

--- src/components/Timers/Timer/Timer.tsx
+++ src/components/Timers/Timer/Timer.tsx
@@ -4,7 +4,17 @@ const Timer = (): JSX.Element => {
     return (
         <div>
             <div className={styles.outerRing}>
-                <div className={styles.innerRing}>00:00</div>
+                <div className={styles.innerRing}>
+                    <div className={styles.timeface}>
+                        <div className={styles.svgContainer}>
+                            <svg xmlns="http://www.w3.org/2000/svg">
+                                <circle cx="50%" cy="50%" r="48%" className={styles.progressBar} />
+                            </svg>
+                        </div>
+                        <p className={styles.time}>00:00</p>
+                        <p className={styles.pause}>PAUSE</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
