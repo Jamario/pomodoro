2b581ba1c52cd7d19529b7d8cb03cf7f3baf264e
bittmario <jamario@bitt.com>
2022-05-02 15:53:43 -0400

    Session2. Added all the timers.

--- src/components/Timers/LongBreak/LongBreak.tsx
+++ src/components/Timers/LongBreak/LongBreak.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
+import Timer from "../Timer";
+import { TimerState } from "../types";
 import styles from "./LongBreak.module.css";
 
 const LongBreak = (): JSX.Element => {
-    return <div className={styles.container}>Long Break Timer</div>;
+    const countdownStartTime = 60; //temporarily
+    const [timerState, setTimerState] = useState(TimerState.Stopped);
+
+    const changeTimerState = (newTimerState: TimerState) => {
+        setTimerState(newTimerState);
+    };
+
+    return (
+        <div className={styles.container}>
+            <Timer
+                countdownStartTime={countdownStartTime}
+                timerState={timerState}
+                changeTimerState={changeTimerState}
+            />
+        </div>
+    );
 };
 
 export default LongBreak;
--- src/components/Timers/ShortBreak/ShortBreak.tsx
+++ src/components/Timers/ShortBreak/ShortBreak.tsx
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
+import Timer from "../Timer";
+import { TimerState } from "../types";
 import styles from "./ShortBreak.module.css";
 
 const ShortBreak = (): JSX.Element => {
-    return <div className={styles.container}>Short Break Timer</div>;
+    const countdownStartTime = 60; //temporarily
+    const [timerState, setTimerState] = useState(TimerState.Stopped);
+
+    /**
+     * the timer has multiple states; paused, running, finished
+     * finished ~> restart
+     * running ~> pause
+     * paused ~> start
+     */
+    const changeTimerState = (newTimerState: TimerState) => {
+        setTimerState(newTimerState);
+    };
+
+    return (
+        <div className={styles.container}>
+            <Timer
+                countdownStartTime={countdownStartTime}
+                timerState={timerState}
+                changeTimerState={changeTimerState}
+            />
+        </div>
+    );
 };
 
 export default ShortBreak;
